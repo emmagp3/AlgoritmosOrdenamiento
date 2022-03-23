8fce69e1b55f989d3b91bafc63058acaca2ae483
Emmanuel Guadalupe <ema_110@outlook.es>
2022-03-23 13:13:30 -0600

    Practica 1 con tiempos medidos

--- QuickSort/quickSort.c
+++ QuickSort/quickSort.c
@@ -1,21 +1,21 @@
 /** 
- * @file treeSort.c
+ * @file quickSort.c
  * @author Valentin Ramos Emmanuel Guadalupe (evalentinr1700@alumno.ipn.mx)
- * @version 2.0
- * @date 2022-03-22
+ * @version 3.0
+ * @date 2022-03-06
  * 
  * @copyright Copyright (c) 2022
  * 
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
 #include <time.h>
 #include "../Utils/archivos.h"
-#include "../Utils/arbol.h"
 #include "../Utils/utils.h"
 
-void treeSort(tipo_dato[], int);
+int pivot(int[], int, int);
+void quickSort(int[], int, int);
+void swap(int[], int, int);
 
 int main(void) {
   clock_t startReal = clock();
@@ -23,11 +23,11 @@ int main(void) {
   clock_t startE = clock();
   int n;
   leerCantidadDeNumeros(&n);
-  tipo_dato *arr = leerNumeros(n);
+  int *arr = leerNumeros(n);
   clock_t endE = clock();
 
   clock_t startCPU = clock();
-  treeSort(arr, n);
+  quickSort(arr, 0, n - 1);
   clock_t endCPU = clock();
 
   clock_t startS = clock();
@@ -48,11 +48,37 @@ int main(void) {
   return 0;
 }
 
-void treeSort(tipo_dato arr[], int n) {
-  int i, j = 0;
-  ArbolBinario a = NULL;
-  for (i = 0; i < n; i++) {
-    a = insertar(a, arr[i]);
+int pivot(int arr[], int p, int r) {
+  int piv = arr[p], i = p + 1, j = r;
+  while (i <= r) {
+    while (arr[i] < piv && i <= r) {
+      i++;
+    }
+    while (arr[j] > piv) {
+      j--;
+    }
+    
+    if (i <= j) {
+      swap(arr, i, j);
+    }
+    else {
+      swap(arr, p, j);
+      return j;
+    } 
   }
-  guardarRecorridoInOrden(a, arr, &j);
+}
+
+void quickSort(int arr[], int p, int r) {
+  int j;
+  if (p < r) {
+    j = pivot(arr, p, r);
+    quickSort(arr, p, j - 1);
+    quickSort(arr, j + 1, r);
+  }
+}
+
+void swap(int arr[], int i, int j) {
+  int temp = arr[j];
+  arr[j] = arr[i];
+  arr[i] = temp;
 }
--- ShellSort/shellSort.c
+++ ShellSort/shellSort.c
@@ -1,26 +1,48 @@
 /** 
  * @file shellSort.c
  * @author Valentin Ramos Emmanuel Guadalupe (evalentinr1700@alumno.ipn.mx)
- * @version 2.0
+ * @version 3.0
  * @date 2022-03-06
  * 
  * @copyright Copyright (c) 2022
  * 
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #include "../Utils/archivos.h"
 #include "../Utils/utils.h"
 
 void shell(int[], int);
 
 int main(void) {
+  clock_t startReal = clock();
+
+  clock_t startE = clock();
   int n;
   leerCantidadDeNumeros(&n);
-  int arr[] = leerNumeros(n);
+  int *arr = leerNumeros(n);
+  clock_t endE = clock();
+  
+  clock_t startCPU = clock();
   shell(arr, n);
-  imprimirArreglo(arr, &n);
+  clock_t endCPU = clock();
 
+  clock_t startS = clock();
+  imprimirArreglo(arr, &n);
+  clock_t endS = clock();
+
+  free(arr);
+
+  clock_t endReal = clock();
+  double realTime = (double) (endReal - startReal) / CLOCKS_PER_SEC;
+  double CPUtime = (double) (endCPU - startCPU) / CLOCKS_PER_SEC;
+  double EStime = (double) ((endE - startE) + (endS - startS)) / CLOCKS_PER_SEC;
+  printf("Tiempo real: %lfs\n", realTime);
+  printf("Tiempo CPU: %lfs\n", CPUtime);
+  printf("Tiempo E/S: %lfs\n", EStime);
+  printf("CPU/Wall: %lf\n", CPUtime / realTime);
   return 0;
 }
 
